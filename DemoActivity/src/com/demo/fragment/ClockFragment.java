421bcccedb2914825ce66661fb289fc6bd910a1a
Dean <396901990@qq.com>
2014-11-17 22:53:04 +0800

    add a demo activity

---
+++ DemoActivity/src/com/demo/fragment/ClockFragment.java
@@ -0,0 +1,30 @@
+package com.demo.fragment;
+
+import com.dean.fragment.CardFragment;
+import com.demo.R;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ClockFragment
+    extends CardFragment
+{
+
+    @Override
+    public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState )
+    {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated( Bundle savedInstanceState )
+    {
+        isShowTitle(false);
+        super.onActivityCreated(savedInstanceState);
+        setCardView(getActivity().getLayoutInflater().inflate(R.layout.clock_card, null, false));
+        
+    }
+
+}
